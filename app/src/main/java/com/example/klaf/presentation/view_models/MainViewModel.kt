79c33cd3a4a9a067a314df694427bb330057a984
NikolayKuts <survivenik@gmail.com>
2021-09-03 21:43:53 +0300

    Add ViewModel for working with decks

---
+++ app/src/main/java/com/example/klaf/presentation/view_models/MainViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.klaf.presentation.view_models
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.klaf.data.implementations.DeckListRepositoryRoomImp
+import com.example.klaf.domain.pojo.Deck
+import com.example.klaf.data.repositories.DeckListRepository
+import kotlinx.coroutines.launch
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: DeckListRepository = DeckListRepositoryRoomImp(application)
+    private var _deckSours = MutableLiveData<List<Deck>>()
+    val deckSours: LiveData<List<Deck>> get() = _deckSours
+
+    init { viewModelScope.launch { _deckSours.value = repository.getDataFormSours() } }
+
+}
