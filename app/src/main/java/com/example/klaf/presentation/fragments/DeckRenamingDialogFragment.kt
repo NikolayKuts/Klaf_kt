0d35d8457885ecf5dbed9855660721f111c5238b
NikolayKuts <survivenik@gmail.com>
2021-12-29 23:42:59 +0300

    Regactor DeckRenamingDialogFragment

--- app/src/main/java/com/example/klaf/presentation/fragments/DeckRenamingDialogFragment.kt
+++ app/src/main/java/com/example/klaf/presentation/fragments/DeckRenamingDialogFragment.kt
@@ -4,23 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.klaf.R
 import com.example.klaf.databinding.DialogDeckRenamingBinding
 import com.example.klaf.domain.pojo.Deck
+import com.example.klaf.domain.showToast
 import com.example.klaf.presentation.view_models.MainViewModel
 
 class DeckRenamingDialogFragment : DialogFragment() {
 
     private var _binding: DialogDeckRenamingBinding? = null
     private val binding get() = _binding!!
-    private val viewModel by activityViewModels<MainViewModel>()
+
     private val args by navArgs<DeckRenamingDialogFragmentArgs>()
+    private val viewModel by activityViewModels<MainViewModel>()
+
+    private var deck: Deck? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,41 +34,45 @@ class DeckRenamingDialogFragment : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val navController = findNavController()
-        var oldDeck: Deck? = null
-        viewModel.onGetDeckById(args.deckId) { deck: Deck? ->
-            oldDeck = deck
+
+
+
+        viewModel.onGetDeckById(args.deckId) { receivedDeck: Deck? ->
+            deck = receivedDeck
             binding.editTextDeckRenamingField.setText(
-                deck?.name ?: getString(R.string.deck_is_not_found)
+                receivedDeck?.name ?: getString(R.string.deck_is_not_found)
             )
         }
 
-        binding.buttonCancelDeckRenaming.setOnClickListener {
-            navController.navigate(R.id.action_deckRenamingDialogFragment_to_deckListFragment)
-        }
-
-        binding.buttonConfirmDeckRenaming.setOnClickListener {
-            oldDeck?.let { deck ->
-                when (val newName = binding.editTextDeckRenamingField.text.toString().trim()) {
-                    "" -> {
-                        Toast.makeText(context, "Type a new name", Toast.LENGTH_SHORT).show()
-                    }
-                    deck.name -> {
-                        Toast.makeText(context, "The name isn't changed", Toast.LENGTH_SHORT).show()
-                    }
-                    else -> {
-                        val newDeck = deck.copy(name = newName)
-                        viewModel.addNewDeck(newDeck)
-//                        findNavController().popBackStack()
-                        navController.navigate(R.id.action_deckRenamingDialogFragment_to_deckListFragment)
-                    }
-                }
-            }
-        }
+        setListeners()
     }
 
     override fun onDestroy() {
         _binding = null
         super.onDestroy()
+    }
+
+    private fun setListeners() {
+        binding.buttonCancelDeckRenaming.setOnClickListener { dismiss() }
+
+        binding.buttonConfirmDeckRenaming.setOnClickListener { onConfirmDeckRenaming() }
+    }
+
+    private fun onConfirmDeckRenaming() {
+        deck?.let { notNullableDeck ->
+            when (val newName = binding.editTextDeckRenamingField.text.toString().trim()) {
+
+                "" -> getString(R.string.type_new_deck_name).showToast(requireContext())
+
+                notNullableDeck.name -> {
+                    getString(R.string.deck_name_is_not_changed).showToast(requireContext())
+                }
+
+                else -> {
+                    viewModel.addNewDeck(notNullableDeck.copy(name = newName))
+                    dismiss()
+                }
+            }
+        }
     }
 }
