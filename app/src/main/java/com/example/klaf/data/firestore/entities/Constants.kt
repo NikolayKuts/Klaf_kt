3e31f4ff823dddf6f04e105a53d77ba79e1fbb22
NikolayKuts <survivenik@gmail.com>
2022-06-27 21:50:01 +0300

    temporary

---
+++ app/src/main/java/com/example/klaf/data/firestore/entities/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.klaf.data.firestore.entities
+
+const val DEFAULT_STRING_VALUE = "empty"
+const val DEFAULT_DOUBLE_VALUE = -1.0
+const val DEFAULT_INT_VALUE = -1
+const val DEFAULT_LONG_VALUE = -1L
+const val DEFAULT_BOOLEAN_VALUE = false
