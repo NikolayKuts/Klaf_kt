a9c3b5e130af29ca8afd6d69f86f5231fc069724
NikolayKuts <survivenik@gmail.com>
2022-07-06 19:01:44 +0300

    Implement retrieving data of old app Klaf

--- app/src/main/java/com/example/klaf/di/DomainModule.kt
+++ app/src/main/java/com/example/klaf/di/DomainModule.kt
@@ -1,9 +1,11 @@
 package com.example.klaf.di
 
+import com.example.klaf.data.OldAppKlafDataTransferRepositoryImpl
 import com.example.klaf.data.room.repositoryImplementations.CardRepositoryRoomImpl
 import com.example.klaf.data.room.repositoryImplementations.DeckRepositoryRoomImp
 import com.example.klaf.domain.repositories.CardRepository
 import com.example.klaf.domain.repositories.DeckRepository
+import com.example.klaf.domain.repositories.OldAppKlafDataTransferRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -18,4 +20,9 @@ interface DomainModule {
 
     @Binds
     fun bindCardRepository(repository: CardRepositoryRoomImpl): CardRepository
+
+    @Binds
+    fun bindOldAppKlafTransferRepository(
+        repository: OldAppKlafDataTransferRepositoryImpl
+    ) : OldAppKlafDataTransferRepository
 }
