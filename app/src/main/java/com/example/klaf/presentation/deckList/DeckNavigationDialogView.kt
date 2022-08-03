31bc82af9f4786114b8b60942fd428bf29d1e875
NikolayKuts <survivenik@gmail.com>
2022-08-03 14:41:04 +0300

    Refactore DeckNavigationDialogView

--- app/src/main/java/com/example/klaf/presentation/deckList/DeckNavigationDialogView.kt
+++ app/src/main/java/com/example/klaf/presentation/deckList/DeckNavigationDialogView.kt
@@ -1,110 +1,92 @@
 package com.example.klaf.presentation.deckList
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.klaf.R
+import com.example.klaf.presentation.common.DIALOG_BUTTON_SIZE
+import com.example.klaf.presentation.common.DialogButton
+import com.example.klaf.presentation.theme.MainTheme
 
 
 @Composable
 fun DeckNavigationDialogView(
     deckName: String,
-    deckId: Int,
     onDeleteDeckClick: () -> Unit,
     onRenameDeckClick: () -> Unit,
     onBrowseDeckClick: () -> Unit,
     onAddCardsClick: () -> Unit,
     onCloseDialogClick: () -> Unit,
 ) {
-    Column() {
-        Box {
-            val closeButtonSize = 50
-
-            Card(
-                modifier = Modifier
-                    .defaultMinSize(minHeight = 300.dp, minWidth = 300.dp)
-                    .padding(bottom = (closeButtonSize / 2).dp)
+    Box {
+        Card(
+            modifier = Modifier
+                .defaultMinSize(minHeight = 300.dp, minWidth = 300.dp)
+                .padding(bottom = (DIALOG_BUTTON_SIZE / 2).dp)
+        ) {
+            Column(
+                Modifier.padding(MainTheme.dimensions.dialogContentPadding)
             ) {
-                Column(
-                    Modifier.padding(32.dp)
-                ) {
-                    Text(
-                        text = deckName,
-                        style = TextStyle(
-                            fontSize = 30.sp,
-                            fontStyle = FontStyle.Italic
-                        ),
-                        modifier = Modifier
-                            .align(alignment = Alignment.CenterHorizontally)
-                            .padding(bottom = 16.dp)
-                    )
-                    Text(
-                        text = "Delete deck",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable { onDeleteDeckClick() }
-                    )
-                    Spacer()
-                    Text(
-                        text = "Rename deck",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable { onRenameDeckClick() }
-                    )
-                    Spacer()
-                    Text(
-                        text = "Browse cards",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable { onBrowseDeckClick() })
-                    Spacer()
-                    Text(
-                        text = "Add cards",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable { onAddCardsClick() }
-                    )
-                }
-            }
-            Card(
-                shape = RoundedCornerShape(50.dp),
-                modifier = Modifier
-                    .align(alignment = Alignment.BottomCenter)
-                    .size(closeButtonSize.dp),
-            ) {
-                Icon(
+                Text(
                     modifier = Modifier
-                        .size(20.dp)
-                        .background(Color(0xFFDB645B))
-                        .clickable { onCloseDialogClick() }
-                        .padding(8.dp),
-                    painter = painterResource(id = R.drawable.ic_close_24),
-                    contentDescription = "",
+                        .align(alignment = Alignment.CenterHorizontally)
+                        .padding(bottom = 16.dp),
+                    text = deckName,
+                    style = MainTheme.typographies.materialTypographies.body1.copy(
+                        fontSize = 30.sp,
+                        fontStyle = FontStyle.Italic
+                    ),
                 )
+
+                DialogItem(textId = R.string.deck_delete, onClick = onDeleteDeckClick)
+                SeparationLine()
+                DialogItem(textId = R.string.rename_deck, onClick = onRenameDeckClick)
+                SeparationLine()
+                DialogItem(textId = R.string.browse_cards, onClick = onBrowseDeckClick)
+                SeparationLine()
+                DialogItem(textId = R.string.add_cards, onClick = onAddCardsClick)
             }
+        }
+        Box(modifier = Modifier.align(alignment = Alignment.BottomCenter)) {
+            DialogButton(
+                background = MainTheme.colors.neutralDialogButton,
+                iconId = R.drawable.ic_close_24,
+                onClick = onCloseDialogClick
+            )
         }
     }
 }
 
 @Composable
-private fun Spacer() {
+private fun DialogItem(
+    @StringRes textId: Int,
+    onClick: () -> Unit,
+) {
+    Text(
+        text = stringResource(id = textId),
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .padding(top = 8.dp, bottom = 8.dp)
+    )
+}
+
+@Composable
+private fun SeparationLine() {
     Spacer(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 8.dp, bottom = 8.dp)
             .height(1.dp)
             .background(Color(0x2DFFFFFF))
     )
--- app/src/main/java/com/example/klaf/presentation/theme/Dimension.kt
+++ app/src/main/java/com/example/klaf/presentation/theme/Dimension.kt
@@ -1,6 +1,8 @@
 package com.example.klaf.presentation.theme
 
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 private const val MAIN_TEXT_SIZE_IN_PIXELS = 16
@@ -10,11 +12,13 @@ data class MainDimensions(
     val deckItemDeckName: TextUnit,
     val deckItemPointer: TextUnit,
     val dialogTextLineHeight: TextUnit,
+    val dialogContentPadding: Dp,
 )
 
 val CommonDimension = MainDimensions(
     mainTextSize = MAIN_TEXT_SIZE_IN_PIXELS.sp,
     deckItemDeckName = 18.sp,
     deckItemPointer = 10.sp,
-    dialogTextLineHeight = (MAIN_TEXT_SIZE_IN_PIXELS + 10).sp
+    dialogTextLineHeight = (MAIN_TEXT_SIZE_IN_PIXELS + 10).sp,
+    dialogContentPadding = 32.dp
 )
