66d19aef89baa4c541d55eecd772fa82657062ad
NikolayKuts <survivenik@gmail.com>
2022-08-03 22:25:07 +0300

    Migrage DeckCreationDialogFragment to Compose

---
+++ app/src/main/java/com/example/klaf/presentation/deckList/deckCreation/DeckCreationDialogView.kt
@@ -0,0 +1,90 @@
+package com.example.klaf.presentation.deckList.deckCreation
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.klaf.R
+import com.example.klaf.presentation.common.DIALOG_BUTTON_SIZE
+import com.example.klaf.presentation.common.DialogButton
+import com.example.klaf.presentation.common.rememberAsMutableStateOf
+import com.example.klaf.presentation.theme.MainTheme
+
+
+@Composable
+fun DeckCreationDialog(
+    onConfirmCreationClick: (deckName: String) -> Unit,
+    onCloseDialogClick: () -> Unit,
+) {
+    val deckNameState = rememberAsMutableStateOf(value = "")
+
+    Box() {
+        Card(
+            modifier = Modifier
+                .defaultMinSize(minHeight = 150.dp, minWidth = 300.dp)
+                .padding(bottom = (DIALOG_BUTTON_SIZE / 2).dp)
+        ) {
+            Column(modifier = Modifier.padding(MainTheme.dimensions.dialogContentPadding)) {
+                DialogTitle()
+                Spacer(modifier = Modifier.height(16.dp))
+                DeckNameTextField(deckNameState = deckNameState)
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(0.5F)
+                .align(alignment = Alignment.BottomCenter),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            ConformationButton(
+                onClick = { onConfirmCreationClick(deckNameState.value) }
+            )
+            DialogClosingButton(onClick = onCloseDialogClick)
+        }
+    }
+}
+
+@Composable
+private fun DialogTitle() {
+    Text(
+        style = MainTheme.typographies.dialogTextStyle,
+        text = stringResource(id = R.string.deck_creation_dialog_title),
+    )
+
+}
+
+@Composable
+private fun DeckNameTextField(deckNameState: MutableState<String>) {
+    OutlinedTextField(
+        value = deckNameState.value,
+        onValueChange = { updatedName -> deckNameState.value = updatedName },
+        label = { Text(text = stringResource(R.string.deck_name_label)) },
+        placeholder = { Text(text = stringResource(id = R.string.enter_deck_name)) },
+    )
+}
+
+@Composable
+private fun ConformationButton(onClick: () -> Unit) {
+    DialogButton(
+        background = MainTheme.colors.positiveDialogButton,
+        iconId = R.drawable.ic_comfirmation_24,
+        onClick = onClick
+    )
+}
+
+@Composable
+private fun DialogClosingButton(onClick: () -> Unit) {
+    DialogButton(
+        background = MainTheme.colors.neutralDialogButton,
+        iconId = R.drawable.ic_close_24,
+        onClick = onClick
+    )
+}
--- app/src/main/java/com/example/klaf/presentation/deckList/deckDeleting/DeckDeletingDialogFragment.kt
+++ app/src/main/java/com/example/klaf/presentation/deckList/deckDeleting/DeckDeletingDialogFragment.kt
@@ -6,26 +6,20 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
 import androidx.compose.ui.platform.ComposeView
-import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.navGraphViewModels
 import com.example.klaf.R
+import com.example.klaf.presentation.common.TransparentDialogFragment
 import com.example.klaf.presentation.deckList.DeckListViewModel
 import com.example.klaf.presentation.theme.MainTheme
 
-class DeckDeletingDialogFragment : DialogFragment(R.layout.dialog_deck_deleting) {
+class DeckDeletingDialogFragment : TransparentDialogFragment(R.layout.dialog_deck_deleting) {
 
     private val args by navArgs<DeckDeletingDialogFragmentArgs>()
     private val navController by lazy { findNavController() }
 
     private val viewModel by navGraphViewModels<DeckListViewModel>(R.id.deckListFragment)
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return super.onCreateDialog(savedInstanceState).apply {
-            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
