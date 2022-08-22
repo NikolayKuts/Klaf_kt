c60bb527aa73b13b8ae87e297c71c1a0f3fef09b
NikolayKuts <survivenik@gmail.com>
2022-08-22 23:50:20 +0300

    Implement remotely saving decks

--- app/src/main/java/com/example/klaf/data/firestore/entities/FirestoreDeck.kt
+++ app/src/main/java/com/example/klaf/data/firestore/entities/FirestoreDeck.kt
@@ -3,13 +3,15 @@ package com.example.klaf.data.firestore.entities
 data class FirestoreDeck(
     val name: String = DEFAULT_STRING_VALUE,
     val creationDate: Long = DEFAULT_LONG_VALUE,
-    val id: Int = DEFAULT_INT_VALUE,
+    val repetitionIterationDates: List<Long> = emptyList(),
+    val scheduledIterationDates: List<Long> = emptyList(),
+    val scheduledDateInterval: Long = DEFAULT_LONG_VALUE,
+    val repetitionQuantity: Int = DEFAULT_INT_VALUE,
     val cardQuantity: Int = DEFAULT_INT_VALUE,
-    val repeatDay: Int = DEFAULT_INT_VALUE,
-    val scheduledDate: Long = DEFAULT_LONG_VALUE,
-    val lastRepeatDate: Long = DEFAULT_LONG_VALUE,
-    val repeatQuantity: Int = DEFAULT_INT_VALUE,
-    val lastRepeatDuration: Long = DEFAULT_LONG_VALUE,
+    val lastFirstRepetitionDuration: Long = DEFAULT_LONG_VALUE,
+    val lastSecondRepetitionDuration: Long = DEFAULT_LONG_VALUE,
+    val lastRepetitionIterationDuration: Long = DEFAULT_LONG_VALUE,
     @field: JvmField
-    val isLastRepetitionSucceeded: Boolean = DEFAULT_BOOLEAN_VALUE,
+    val isLastIterationSucceeded: Boolean = DEFAULT_BOOLEAN_VALUE,
+    val id: Int = DEFAULT_INT_VALUE,
 )
--- app/src/main/java/com/example/klaf/domain/useCases/DeleteCardFromDeckUseCase.kt
+++ app/src/main/java/com/example/klaf/domain/useCases/DeleteCardFromDeckUseCase.kt
@@ -1,5 +1,6 @@
 package com.example.klaf.domain.useCases
 
+import com.example.klaf.di.DeckRepositoryRoomImp
 import com.example.klaf.domain.repositories.CardRepository
 import com.example.klaf.domain.repositories.DeckRepository
 import kotlinx.coroutines.Dispatchers
@@ -7,6 +8,7 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class DeleteCardFromDeckUseCase @Inject constructor(
+    @DeckRepositoryRoomImp
     private val deckRepository: DeckRepository,
     private val cardRepository: CardRepository,
 ) {
