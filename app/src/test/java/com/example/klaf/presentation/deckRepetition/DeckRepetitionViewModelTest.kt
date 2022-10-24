8ebc1060b642dd65a57b5ddfe7ab84ad3862c92a
NikolayKuts <survivenik@gmail.com>
2022-10-25 00:16:10 +0300

    Add test for getting a message on fetching cards

--- app/src/test/java/com/example/klaf/presentation/deckRepetition/DeckRepetitionViewModelTest.kt
+++ app/src/test/java/com/example/klaf/presentation/deckRepetition/DeckRepetitionViewModelTest.kt
@@ -1,27 +1,22 @@
 package com.example.klaf.presentation.deckRepetition
 
-import androidx.annotation.StringRes
 import androidx.work.WorkManager
-import app.cash.turbine.test
 import com.example.klaf.R
 import com.example.klaf.common.MainDispatcherRule
 import com.example.klaf.common.launchEventMassageIdEqualsTest
 import com.example.klaf.data.networking.CardAudioPlayer
-import com.example.klaf.domain.useCases.*
+import com.example.klaf.domain.useCases.DeleteCardFromDeckUseCase
+import com.example.klaf.domain.useCases.FetchCardsUseCase
+import com.example.klaf.domain.useCases.FetchDeckByIdUseCase
+import com.example.klaf.domain.useCases.UpdateDeckUseCase
 import com.example.klaf.presentation.common.RepetitionTimer
-import com.example.klaf.presentation.deckList.common.DeckListViewModel
-import dagger.assisted.Assisted
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 
@@ -51,6 +46,26 @@ class DeckRepetitionViewModelTest {
         )
     }
 
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `getting problem message when fetching cards is failed`() = runTest() {
+        val deckId = 111111
+        val fetchCardsUseCase: FetchCardsUseCase = mockk() {
+            every { invoke(deckId = deckId) } returns flow { throw Exception() }
+        }
+
+        val viewModel = createViewModel(
+            deckId = deckId,
+            fetchCards = fetchCardsUseCase
+        )
+
+        verify(exactly = 1) { fetchCardsUseCase.invoke(deckId = deckId) }
+
+        launchEventMassageIdEqualsTest(
+            viewModel = viewModel,
+            expectedMassageId = R.string.problem_with_fetching_cards
+        )
+    }
 
     private fun createViewModel(
         deckId: Int = mockk(relaxed = true),
