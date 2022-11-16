9b467d19e6e9d955b1a3214a557d4dd5e8b2bb69
NikolayKuts <survivenik@gmail.com>
2022-11-16 19:35:20 +0200

    Define main entities for working with DeckRepetitionInfo

---
+++ app/src/main/java/com/example/klaf/data/dataStore/DeckRepetitionInfo.kt
@@ -0,0 +1,15 @@
+package com.example.klaf.data.dataStore
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DeckRepetitionInfo(
+    val deckId: Int
+//    val currentDuration: String,
+//    val previousDuration: String,
+//    val scheduledDate: Long,
+//    val previousScheduledDate: Long,
+//    val lastRepetitionIterationDate: String?,
+//    val repetitionQuantity: String,
+//    val lastSuccessMark: String,
+)
---
+++ app/src/main/java/com/example/klaf/data/dataStore/DeckRepetitionInfosSerializer.kt
@@ -0,0 +1,28 @@
+package com.example.klaf.data.dataStore
+
+import androidx.datastore.core.Serializer
+import kotlinx.serialization.json.Json
+import java.io.InputStream
+import java.io.OutputStream
+
+@Suppress("BlockingMethodInNonBlockingContext")
+object DeckRepetitionInfosSerializer : Serializer<DeckRepetitionInfos> {
+
+    override val defaultValue: DeckRepetitionInfos = DeckRepetitionInfos()
+
+    override suspend fun readFrom(input: InputStream): DeckRepetitionInfos {
+        return Json.decodeFromString(
+            deserializer = DeckRepetitionInfos.serializer(),
+            string = input.readBytes().decodeToString()
+        )
+    }
+
+    override suspend fun writeTo(t: DeckRepetitionInfos, output: OutputStream) {
+        output.write(
+            Json.encodeToString(
+                serializer = DeckRepetitionInfos.serializer(),
+                value = t
+            ).encodeToByteArray()
+        )
+    }
+}
