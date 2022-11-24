8ca6b191b59950dd3605eabae02a118ca835ef15
NikolayKuts <survivenik@gmail.com>
2022-11-24 13:37:29 +0200

    Add the base elements to the screen

--- app/src/main/java/com/example/klaf/presentation/interimDeck/BaseInterimDeckViewModel.kt
+++ app/src/main/java/com/example/klaf/presentation/interimDeck/BaseInterimDeckViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.klaf.presentation.interimDeck
 
 import androidx.lifecycle.ViewModel
-import com.example.klaf.domain.entities.Card
 import com.example.klaf.domain.entities.Deck
 import com.example.klaf.presentation.common.EventMessageSource
 import kotlinx.coroutines.flow.SharedFlow
@@ -10,7 +9,6 @@ import kotlinx.coroutines.flow.StateFlow
 abstract class BaseInterimDeckViewModel : ViewModel(), EventMessageSource {
 
     abstract val interimDeck: SharedFlow<Deck?>
-    abstract val cards: StateFlow<List<Card>>
     abstract val cardHolders: StateFlow<List<SelectableCardHolder>>
 
     abstract fun changeSelectionState(position: Int)
