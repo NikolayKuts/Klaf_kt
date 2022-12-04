f7bb79ca3797fd174350a9a5f9006762b7940c9c
NikolayKuts <survivenik@gmail.com>
2022-12-04 23:14:47 +0200

    Change the work logic of CardAdditionViewModel and CardAdditionScreen

---
+++ app/src/main/java/com/example/klaf/presentation/cardAddition/CardAdditionEvent.kt
@@ -0,0 +1,25 @@
+package com.example.klaf.presentation.cardAddition
+
+import com.example.klaf.domain.ipa.LetterInfo
+
+sealed interface CardAdditionEvent {
+
+    data class UpdateNativeWord(val word: String) : CardAdditionEvent
+
+    data class UpdateForeignWordWithIpaTemplate(val word: String) : CardAdditionEvent
+
+    data class UpdateIpaTemplate(val ipa: String) : CardAdditionEvent
+
+    data class ChangeLetterSelectionWithIpaTemplate(
+        val index: Int,
+        val letterInfo: LetterInfo
+    ) : CardAdditionEvent
+
+    data class  AddNewCard(
+        val deckId: Int,
+        val nativeWord: String,
+        val foreignWord: String,
+        val letterInfos: List<LetterInfo>,
+        val ipaTemplate: String,
+    ) : CardAdditionEvent
+}
--- app/src/main/java/com/example/klaf/presentation/cardAddition/CardAdditionViewModel.kt
+++ app/src/main/java/com/example/klaf/presentation/cardAddition/CardAdditionViewModel.kt
@@ -3,13 +3,17 @@ package com.example.klaf.presentation.cardAddition
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.klaf.R
+import com.example.klaf.domain.common.generateLetterInfos
 import com.example.klaf.domain.common.launchWithExceptionHandler
 import com.example.klaf.domain.entities.Card
 import com.example.klaf.domain.entities.Deck
 import com.example.klaf.domain.ipa.LetterInfo
 import com.example.klaf.domain.ipa.convertToEncodedIpa
+import com.example.klaf.domain.ipa.convertToUncompletedIpa
+import com.example.klaf.domain.ipa.toWord
 import com.example.klaf.domain.useCases.AddNewCardIntoDeckUseCase
 import com.example.klaf.domain.useCases.FetchDeckByIdUseCase
+import com.example.klaf.presentation.cardAddition.CardAdditionEvent.*
 import com.example.klaf.presentation.common.EventMessage
 import com.example.klaf.presentation.common.tryEmit
 import dagger.assisted.Assisted
@@ -18,6 +22,7 @@ import kotlinx.coroutines.flow.*
 
 class CardAdditionViewModel @AssistedInject constructor(
     @Assisted deckId: Int,
+    @Assisted smartSelectedWord: String?,
     fetchDeckById: FetchDeckByIdUseCase,
     private val addNewCardIntoDeck: AddNewCardIntoDeckUseCase,
 ) : ViewModel() {
@@ -33,17 +38,65 @@ class CardAdditionViewModel @AssistedInject constructor(
             replay = 1
         )
 
-    private val _cardAdditionState = MutableStateFlow(value = CardAdditionState.NOT_ADDED)
+    private val _cardAdditionState = MutableStateFlow<CardAdditionState>(
+        value = CardAdditionState.Adding(
+            letterInfos = smartSelectedWord?.generateLetterInfos() ?: emptyList(),
+            nativeWord = "",
+            foreignWord = "",
+            ipaTemplate = ""
+        )
+    )
     val cardAdditionState = _cardAdditionState.asStateFlow()
 
-    fun addNewCard(
+    private val letterInfosState = MutableStateFlow(value = _cardAdditionState.value.letterInfos)
+    private val nativeWordState = MutableStateFlow(value = _cardAdditionState.value.nativeWord)
+    private val ipaTemplateState = MutableStateFlow(value = _cardAdditionState.value.ipaTemplate)
+
+    init {
+        combine(
+            letterInfosState,
+            nativeWordState,
+            ipaTemplateState
+        ) { letterInfos, nativeWord, ipaTemplate ->
+            CardAdditionState.Adding(
+                letterInfos = letterInfos,
+                nativeWord = nativeWord,
+                foreignWord = letterInfos.toWord(),
+                ipaTemplate = ipaTemplate
+            )
+        }.onEach { addingState -> _cardAdditionState.value = addingState }
+            .launchIn(viewModelScope)
+
+    }
+
+    fun sendEvent(event: CardAdditionEvent) {
+        when (event) {
+            is ChangeLetterSelectionWithIpaTemplate -> {
+                changeLetterSelectionWithIpaTemplate(index = event.index, letterInfo = event.letterInfo)
+            }
+            is UpdateForeignWordWithIpaTemplate -> updateForeignWordWithIpaTemplate(word = event.word)
+            is UpdateIpaTemplate -> updateIpa(ipa = event.ipa)
+            is UpdateNativeWord -> updateNativeWord(word = event.word)
+            is AddNewCard -> {
+                addNewCard(
+                    deckId = event.deckId,
+                    nativeWord = event.nativeWord,
+                    foreignWord = event.foreignWord,
+                    letterInfos = event.letterInfos,
+                    ipaTemplate = event.ipaTemplate
+                )
+            }
+        }
+    }
+
+    private fun addNewCard(
         deckId: Int,
         nativeWord: String,
         foreignWord: String,
         letterInfos: List<LetterInfo>,
         ipaTemplate: String,
     ) {
-        if (nativeWord.isEmpty() || foreignWord.isEmpty()) {
+        if (nativeWordState.value.isEmpty() || foreignWord.isEmpty()) {
             _eventMessage.tryEmit(
                 messageId = R.string.native_and_foreign_words_must_be_filled
             )
@@ -60,17 +113,38 @@ class CardAdditionViewModel @AssistedInject constructor(
                     _eventMessage.tryEmit(messageId = R.string.exception_adding_card)
                 },
                 onCompletion = {
+                    _cardAdditionState.value = CardAdditionState.Finished
                     _eventMessage.tryEmit(messageId = R.string.card_has_been_added)
-                }
-            ) {
-                addNewCardIntoDeck(card = newCard)
-            }
-
-            _cardAdditionState.value = CardAdditionState.ADDED
+                },
+                task = { addNewCardIntoDeck(card = newCard) }
+            )
         }
     }
 
-    fun resetCardAdditionState() {
-        _cardAdditionState.value = CardAdditionState.NOT_ADDED
+    private fun changeLetterSelectionWithIpaTemplate(index: Int, letterInfo: LetterInfo) {
+        val updatedIsChecked = when (letterInfo.letter) {
+            " " -> false
+            else -> !letterInfo.isChecked
+        }
+
+        letterInfosState.update { infos ->
+            infos.toMutableList()
+                .apply { this[index] = letterInfo.copy(isChecked = updatedIsChecked) }
+        }
+
+        ipaTemplateState.value = letterInfosState.value.convertToUncompletedIpa()
+    }
+
+    private fun updateNativeWord(word: String) {
+        nativeWordState.value = word
+    }
+
+    private fun updateForeignWordWithIpaTemplate(word: String) {
+        letterInfosState.value = word.generateLetterInfos()
+        ipaTemplateState.value = letterInfosState.value.convertToUncompletedIpa()
+    }
+
+    private fun updateIpa(ipa: String) {
+        ipaTemplateState.value = ipa
     }
 }
