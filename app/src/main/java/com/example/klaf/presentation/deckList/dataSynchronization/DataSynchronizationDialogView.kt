2456b70209f4a15c7bcd2d6a5cbb5d4952dedc6e
NikolayKuts <survivenik@gmail.com>
2022-12-04 14:17:37 +0200

    Refactor the UI of data synchronization dialog

--- app/src/main/java/com/example/klaf/presentation/deckList/dataSynchronization/DataSynchronizationDialogView.kt
+++ app/src/main/java/com/example/klaf/presentation/deckList/dataSynchronization/DataSynchronizationDialogView.kt
@@ -1,5 +1,6 @@
 package com.example.klaf.presentation.deckList.dataSynchronization
 
+import android.annotation.SuppressLint
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
@@ -12,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
@@ -22,45 +22,35 @@ import androidx.compose.ui.unit.dp
 import com.example.klaf.R
 import com.example.klaf.data.common.DataSynchronizationState.*
 import com.example.klaf.presentation.common.DIALOG_BUTTON_SIZE
+import com.example.klaf.presentation.common.FullBackgroundDialog
 import com.example.klaf.presentation.common.RoundButton
+import com.example.klaf.presentation.common.noRippleClickable
 import com.example.klaf.presentation.deckList.common.BaseDeckListViewModel
 import com.example.klaf.presentation.theme.MainTheme
 
 @Composable
 fun DataSynchronizationDialogView(
     viewModel: BaseDeckListViewModel,
-    onClose: () -> Unit,
+    onCloseClick: () -> Unit,
 ) {
     val synchronizationState by viewModel.dataSynchronizationState.collectAsState()
-    Box {
-        val cardModifier = Modifier
-            .defaultMinSize(minHeight = 150.dp, minWidth = 300.dp)
-            .padding(
-                top = (DIALOG_BUTTON_SIZE / 2).dp,
-                bottom = (DIALOG_BUTTON_SIZE / 2).dp
-            )
 
-        when (synchronizationState) {
-            UncertainState -> {}
-            InitialState -> {
-                InitialStateView(
-                    modifier = cardModifier,
-                    onClose = onClose,
-                    onConfirm = { viewModel.synchronizeData() }
-                )
-            }
-            is SynchronizingState -> {
-                SynchronizationStateView(
-                    modifier = cardModifier,
-                    synchronizationData = (synchronizationState as? SynchronizingState)
-                        ?.synchronizationData ?: ""
-                )
-            }
-            FinishedState -> {
-                FinishStateView(modifier = cardModifier, onClose = onClose)
-            }
+    when (val state = synchronizationState) {
+        UncertainState -> {}
+        InitialState -> {
+            InitialStateView(
+                onCloseClick = onCloseClick,
+                onConfirmClick = { viewModel.synchronizeData() }
+            )
+        }
+        is SynchronizingState -> {
+            SynchronizationStateView(synchronizationData = state.synchronizationData)
+        }
+        FinishedState -> {
+            FinishStateView(onCloseClick = onCloseClick)
         }
     }
+
     DisposableEffect(
         key1 = null,
         effect = {
@@ -70,58 +60,41 @@ fun DataSynchronizationDialogView(
 }
 
 @Composable
-private fun BoxScope.InitialStateView(
-    modifier: Modifier,
-    onClose: () -> Unit,
-    onConfirm: () -> Unit,
+private fun InitialStateView(
+    onCloseClick: () -> Unit,
+    onConfirmClick: () -> Unit,
 ) {
-    Card(modifier = modifier) {
-        Box(
-            modifier = Modifier
-                .padding(MainTheme.dimensions.dialogContentPadding)
-                .padding(top = 8.dp)
-                .padding(bottom = 16.dp)
-        ) {
+    FullBackgroundDialog(
+        onBackgroundClick = onCloseClick,
+        topContent = { SynchronizationLabel() },
+        mainContent = {
             Text(
                 style = MainTheme.typographies.dialogTextStyle,
                 text = stringResource(R.string.data_synchronization_dialog_title)
             )
+        },
+        buttonContent = {
+            RoundButton(
+                background = MainTheme.colors.positiveDialogButton,
+                iconId = R.drawable.ic_confirmation_24,
+                onClick = onConfirmClick
+            )
+
+            RoundButton(
+                background = MainTheme.colors.neutralDialogButton,
+                iconId = R.drawable.ic_close_24,
+                onClick = onCloseClick
+            )
         }
-    }
-
-    SynchronizationLabel()
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(0.5F)
-            .align(alignment = Alignment.BottomCenter),
-        horizontalArrangement = Arrangement.SpaceAround
-    ) {
-        RoundButton(
-            background = MainTheme.colors.positiveDialogButton,
-            iconId = R.drawable.ic_confirmation_24,
-            onClick = onConfirm
-        )
-
-        RoundButton(
-            background = MainTheme.colors.neutralDialogButton,
-            iconId = R.drawable.ic_close_24,
-            onClick = onClose
-        )
-    }
+    )
 }
 
 @Composable
-private fun BoxScope.SynchronizationStateView(
-    modifier: Modifier,
-    synchronizationData: String,
-) {
-    Card(modifier = modifier) {
-        Column(
-            modifier = Modifier
-                .padding(MainTheme.dimensions.dialogContentPadding)
-                .padding(top = 8.dp)
-        ) {
+private fun SynchronizationStateView(synchronizationData: String) {
+    FullBackgroundDialog(
+        onBackgroundClick = { },
+        topContent = { AnimatedSynchronizationLabel() },
+        mainContent = {
             Text(
                 style = MainTheme.typographies.dialogTextStyle,
                 text = stringResource(R.string.data_synchronization_dialog_sync_state_text)
@@ -133,65 +106,37 @@ private fun BoxScope.SynchronizationStateView(
                 overflow = TextOverflow.Ellipsis,
                 maxLines = 1
             )
-        }
-    }
-
-    AnimatedSynchronizationLabel()
-}
-
-@Composable
-private fun BoxScope.FinishStateView(
-    modifier: Modifier,
-    onClose: () -> Unit,
-) {
-    Card(modifier = modifier) {
-        Box(
-            modifier = Modifier
-                .padding(MainTheme.dimensions.dialogContentPadding)
-                .padding(top = 8.dp)
-                .padding(bottom = 16.dp)
-        ) {
-            Text(
-                style = MainTheme.typographies.dialogTextStyle,
-                text = stringResource(R.string.data_synchronization_dialog_finish_state_text)
-            )
-        }
-    }
-    SynchronizationLabel()
-
-    RoundButton(
-        modifier = Modifier.align(alignment = Alignment.BottomCenter),
-        background = MainTheme.colors.neutralDialogButton,
-        iconId = R.drawable.ic_close_24,
-        onClick = onClose
+        },
     )
 }
 
 @Composable
-private fun BoxScope.SynchronizationLabel(iconModifier: Modifier = Modifier) {
-    Card(
-        modifier = Modifier
-            .size(DIALOG_BUTTON_SIZE.dp)
-            .align(alignment = Alignment.TopCenter),
-        shape = RoundedCornerShape(DIALOG_BUTTON_SIZE.dp),
-        elevation = 0.dp,
-    ) {
-        Icon(
-            modifier = iconModifier
-                .size(20.dp)
-                .background(MainTheme.colors.dataSynchronizationViewColors.labelBackground)
-                .padding(8.dp),
-            painter = painterResource(id = R.drawable.ic_sync_24),
-            contentDescription = null,
-        )
-    }
+private fun FinishStateView(onCloseClick: () -> Unit) {
+    FullBackgroundDialog(
+        onBackgroundClick = onCloseClick,
+        topContent = { SynchronizationLabel() },
+        mainContent = {
+            Text(
+                style = MainTheme.typographies.dialogTextStyle,
+                text = stringResource(R.string.data_synchronization_dialog_finish_state_text)
+            )
+        },
+        buttonContent = {
+            RoundButton(
+                background = MainTheme.colors.neutralDialogButton,
+                iconId = R.drawable.ic_close_24,
+                onClick = onCloseClick
+            )
+        }
+    )
 }
 
 @Composable
-private fun BoxScope.AnimatedSynchronizationLabel() {
+private fun AnimatedSynchronizationLabel() {
     val animationDuration = 1000
     val infiniteTransition = rememberInfiniteTransition()
-    val rotation by infiniteTransition.animateFloat(initialValue = 0F,
+    val rotation by infiniteTransition.animateFloat(
+        initialValue = 0F,
         targetValue = -360F,
         animationSpec = infiniteRepeatable(
             animation = tween(
@@ -213,19 +158,32 @@ private fun BoxScope.AnimatedSynchronizationLabel() {
         )
     )
 
+    SynchronizationLabel(
+        modifier = Modifier
+            .rotate(degrees = rotation)
+            .background(color = color)
+            .size(20.dp)
+            .padding(8.dp),
+    )
+}
+
+@SuppressLint("ModifierParameter")
+@Composable
+private fun SynchronizationLabel(
+    modifier: Modifier = Modifier
+        .size(20.dp)
+        .background(MainTheme.colors.dataSynchronizationViewColors.labelBackground)
+        .padding(8.dp),
+) {
     Card(
         modifier = Modifier
             .size(DIALOG_BUTTON_SIZE.dp)
-            .align(alignment = Alignment.TopCenter),
+            .noRippleClickable { },
         shape = RoundedCornerShape(DIALOG_BUTTON_SIZE.dp),
         elevation = 0.dp,
     ) {
         Icon(
-            modifier = Modifier
-                .rotate(degrees = rotation)
-                .background(color = color)
-                .size(20.dp)
-                .padding(8.dp),
+            modifier = modifier,
             painter = painterResource(id = R.drawable.ic_sync_24),
             contentDescription = null,
         )
