77b852c220d4b9dfa2b4ffa7954bc62fbc769562
NikolayKuts <survivenik@gmail.com>
2023-01-01 21:12:43 +0200

    ResSolve word case issue on autocompleteÐ

--- app/src/main/java/com/example/klaf/data/firestore/repositoryImplementations/WordAutocompleteFirestoreImp.kt
+++ app/src/main/java/com/example/klaf/data/firestore/repositoryImplementations/WordAutocompleteFirestoreImp.kt
@@ -20,28 +20,30 @@ class WordAutocompleteFirestoreImp @Inject constructor(
         private const val WORD_SUB_COLLECTION_SUFFIX = "_subCollection"
         private const val UNICODE_RANGE = "\uf8ff"
         private const val RESULT_LIMIT = 10L
-        private const val MIN_WORD_LENGTH = 2
+        private const val MIN_WORD_LENGTH = 1
     }
 
     override suspend fun fetchAutocomplete(prefix: String): List<AutocompleteWord> {
-        firestore.collection(AUTOCOMPLETE_WORD_COLLECTION_NAME)
-        val wordFieldName = FirestoreAutocompleteWord::word.name
-        val groupLetter = prefix.firstOrNull()
+        val groupLetter = prefix.trim().firstOrNull()?.lowercase()
 
-        return if (groupLetter == null) {
+        return if (groupLetter.isNullOrEmpty()) {
             emptyList()
         } else {
+            val wordFieldName = FirestoreAutocompleteWord::word.name
+            val formattedPrefix = prefix.trim().lowercase()
+
             firestore.collection(AUTOCOMPLETE_WORD_COLLECTION_NAME)
                 .document("${groupLetter}$WORD_GROUP_SUFFIX")
                 .collection("${groupLetter}$WORD_SUB_COLLECTION_SUFFIX")
                 .orderBy(wordFieldName)
-                .startAt(prefix)
-                .endAt("$prefix$UNICODE_RANGE")
+                .startAt(formattedPrefix)
+                .endAt("$formattedPrefix$UNICODE_RANGE")
                 .limit(RESULT_LIMIT)
                 .get()
                 .await()
                 .documents.mapNotNull { document -> document.toObject<FirestoreAutocompleteWord>() }
                 .map { firestoreAutocompleteWord -> firestoreAutocompleteWord.mapToDomainEntity() }
+                .map { autocompleteWord -> AutocompleteWord(value = autocompleteWord.value.trim()) }
                 .filter { autocompleteWord -> autocompleteWord.value.length > MIN_WORD_LENGTH }
         }
     }
