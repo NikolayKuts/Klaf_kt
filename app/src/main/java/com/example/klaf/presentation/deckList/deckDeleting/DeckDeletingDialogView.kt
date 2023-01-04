63dee90dfa9381b64ce3865821b0387e7e8e80d2
NikolayKuts <survivenik@gmail.com>
2023-01-04 17:53:00 +0200

    Refactor DeckDeletingDialogView

--- app/src/main/java/com/example/klaf/presentation/deckList/deckDeleting/DeckDeletingDialogView.kt
+++ app/src/main/java/com/example/klaf/presentation/deckList/deckDeleting/DeckDeletingDialogView.kt
@@ -1,21 +1,15 @@
 package com.example.klaf.presentation.deckList.deckDeleting
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.dp
 import com.example.klaf.R
-import com.example.klaf.presentation.common.DIALOG_BUTTON_SIZE
+import com.example.klaf.presentation.common.FullBackgroundDialog
 import com.example.klaf.presentation.common.RoundButton
 import com.example.klaf.presentation.theme.MainTheme
-
 
 @Composable
 fun DeckDeletionDialogView(
@@ -23,38 +17,27 @@ fun DeckDeletionDialogView(
     onCloseDialogButtonClick: () -> Unit,
     onConfirmDeckDeletingButtonClick: () -> Unit,
 ) {
-    Box {
-        Card(
-            modifier = Modifier
-                .defaultMinSize(minHeight = 150.dp, minWidth = 300.dp)
-                .padding(bottom = (DIALOG_BUTTON_SIZE / 2).dp)
-        ) {
-            Box(modifier = Modifier.padding(MainTheme.dimensions.dialogContentPadding)) {
-                Text(
-                    style = MainTheme.typographies.dialogTextStyle,
-                    text = buildAnnotatedString {
-                        withStyle(style = SpanStyle()) {
-                            append(stringResource(R.string.deck_deleting_title))
-                        }
-                        withStyle(style = MainTheme.typographies.accentedDialogText) {
-                            append(" \"${deckName}\"")
-                        }
-                        withStyle(style = SpanStyle()) { append("?") }
-                    },
-                )
-            }
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(0.5F)
-                .align(alignment = Alignment.BottomCenter),
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
+    FullBackgroundDialog(
+        onBackgroundClick = onCloseDialogButtonClick,
+        mainContent = {
+            Text(
+                style = MainTheme.typographies.dialogTextStyle,
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle()) {
+                        append(stringResource(R.string.deck_deleting_title))
+                    }
+                    withStyle(style = MainTheme.typographies.accentedDialogText) {
+                        append(" \"${deckName}\"")
+                    }
+                    withStyle(style = SpanStyle()) { append("?") }
+                },
+            )
+        },
+        bottomContent = {
             DeckDeletingConformationButton(onClick = onConfirmDeckDeletingButtonClick)
             DialogClosingButton(onClick = onCloseDialogButtonClick)
         }
-
-    }
+    )
 }
 
 @Composable
