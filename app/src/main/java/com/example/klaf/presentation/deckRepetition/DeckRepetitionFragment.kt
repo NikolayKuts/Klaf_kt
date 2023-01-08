8a297e2cc2dd2829ca7be9718495643d97aaf3d1
NikolayKuts <survivenik@gmail.com>
2023-01-08 11:13:47 +0200

    Implement removing Ña deck repetition notification when the repetition iteration is finished

--- app/src/main/java/com/example/klaf/presentation/deckRepetition/DeckRepetitionFragment.kt
+++ app/src/main/java/com/example/klaf/presentation/deckRepetition/DeckRepetitionFragment.kt
@@ -10,7 +10,6 @@ import androidx.navigation.fragment.navArgs
 import androidx.navigation.navGraphViewModels
 import com.example.klaf.R
 import com.example.klaf.presentation.common.collectWhenStarted
-import com.example.klaf.presentation.common.showToast
 import com.example.klaf.presentation.theme.MainTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
