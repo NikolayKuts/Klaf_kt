c0701c2f7c737d25b576f8664e1f66137ab87d9d
NikolayKuts <survivenik@gmail.com>
2023-05-11 16:02:04 +0300

    Simplify CardDeletingDialogFragment

--- app/src/main/java/com/example/klaf/presentation/cardTransferring/cardDeleting/CardDeletingDialogFragment.kt
+++ app/src/main/java/com/example/klaf/presentation/cardTransferring/cardDeleting/CardDeletingDialogFragment.kt
@@ -8,62 +8,40 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.navGraphViewModels
-import com.example.domain.common.LoadingState
 import com.example.klaf.R
 import com.example.klaf.presentation.cardTransferring.common.BaseCardTransferringViewModel
 import com.example.klaf.presentation.common.CardDeletingDialogView
 import com.example.klaf.presentation.common.TransparentDialogFragment
-import com.example.klaf.presentation.common.TransparentSurface
-import com.example.klaf.presentation.common.collectWhenStarted
 import com.example.klaf.presentation.theme.MainTheme
 
 class CardDeletingDialogFragment : TransparentDialogFragment(
     layoutId = R.layout.common_compose_layout,
 ) {
 
+    private val args by navArgs<CardDeletingDialogFragmentArgs>()
+
     private val viewModel by navGraphViewModels<BaseCardTransferringViewModel>(
         navGraphId = R.id.cardTransferringFragment
     )
-    private val args by navArgs<CardDeletingDialogFragmentArgs>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         view.findViewById<ComposeView>(R.id.compose_view).setContent {
             MainTheme {
-                TransparentSurface {
-                    val eventMessage by sharedViewModel.eventMessage.collectAsState(initial = null)
+                val eventMessage by sharedViewModel.eventMessage.collectAsState(initial = null)
 
-                    CardDeletingDialogView(
-                        cardQuantity = args.cardQuantity,
-                        onConfirmDeleting = ::deleteCards,
-                        onCancel = ::closeDialog,
-                        eventMessage = eventMessage
-                    )
-                }
-            }
-        }
-
-        observeCardDeletingState()
-    }
-
-    private fun observeCardDeletingState() {
-        viewModel.cardsDeletingState.collectWhenStarted(
-            lifecycleOwner = viewLifecycleOwner
-        ) { deletingState ->
-            when (deletingState) {
-                is LoadingState.Success -> closeDialog()
-                else -> {}
+                CardDeletingDialogView(
+                    cardQuantity = args.cardQuantity,
+                    onConfirmDeleting = ::deleteCards,
+                    onCancel = findNavController()::popBackStack,
+                    eventMessage = eventMessage
+                )
             }
         }
     }
 
     private fun deleteCards() {
         viewModel.deleteCards()
-    }
-
-    private fun closeDialog() {
-        findNavController().popBackStack()
     }
 }
