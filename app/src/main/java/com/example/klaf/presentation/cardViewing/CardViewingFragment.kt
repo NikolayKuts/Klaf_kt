8aef1cba20c66b66bfc439c292dfae690f3c73de
NikolayKuts <survivenik@gmail.com>
2023-05-12 11:00:19 +0300

    Intergrate custom notifying

--- app/src/main/java/com/example/klaf/presentation/cardViewing/CardViewingFragment.kt
+++ app/src/main/java/com/example/klaf/presentation/cardViewing/CardViewingFragment.kt
@@ -4,18 +4,17 @@ import android.os.Bundle
 import android.view.View
 import androidx.compose.material.Surface
 import androidx.compose.ui.platform.ComposeView
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.example.klaf.R
+import com.example.klaf.presentation.common.BaseFragment
 import com.example.klaf.presentation.common.collectWhenStarted
-import com.example.klaf.presentation.common.showSnackBar
 import com.example.klaf.presentation.theme.MainTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class CardViewingFragment : Fragment(R.layout.common_compose_layout) {
+class CardViewingFragment : BaseFragment(layoutId = R.layout.common_compose_layout) {
 
     private val args by navArgs<CardViewingFragmentArgs>()
 
@@ -28,7 +27,7 @@ class CardViewingFragment : Fragment(R.layout.common_compose_layout) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        observeEventMessage(view = view)
+        observeEventMessage()
 
         view.findViewById<ComposeView>(R.id.compose_view).setContent {
             MainTheme {
@@ -37,11 +36,11 @@ class CardViewingFragment : Fragment(R.layout.common_compose_layout) {
         }
     }
 
-    private fun observeEventMessage(view: View) {
+    private fun observeEventMessage() {
         viewModel.eventMessage.collectWhenStarted(
             lifecycleOwner = viewLifecycleOwner
         ) { eventMessage ->
-            view.showSnackBar(messageId = eventMessage.resId)
+            sharedViewModel.notify(message = eventMessage)
         }
     }
 }
