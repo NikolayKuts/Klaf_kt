a8dffa72485fefcef3c671ff42753c7afddb3864
NikolayKuts <survivenik@gmail.com>
2024-08-21 23:32:41 +0500

    Version upgrade to 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 25
         targetSdk = 33
         versionCode = 1
-        versionName = "1.2"
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
