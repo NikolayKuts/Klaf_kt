67c2af86f7de40700febb7c8a63020f74c1b2637
NikolayKuts <survivenik@gmail.com>
2024-10-04 21:39:03 +0500

    Version upgrade to 1.5 (10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
         /** Version **/
         versionName = "1.5"
-        versionCode = 9
+        versionCode = 10
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
