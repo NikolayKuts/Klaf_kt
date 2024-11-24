a6eba00eedf59bf652a9e0c3f483a6974277012e
NikolayKuts <survivenik@gmail.com>
2024-11-24 11:22:18 +0500

    Refactor building word pronunciation audio url

--- app/src/main/java/com/kuts/klaf/data/networking/CardAudioPlayer.kt
+++ app/src/main/java/com/kuts/klaf/data/networking/CardAudioPlayer.kt
@@ -2,6 +2,7 @@ package com.kuts.klaf.data.networking
 
 import android.media.AudioAttributes
 import android.media.MediaPlayer
+import android.net.Uri
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import com.kuts.domain.common.CoroutineStateHolder.Companion.launchWithState
@@ -144,6 +145,10 @@ class CardAudioPlayer @Inject constructor(
 
             delay(500)
             reset()
+
+            val builtAudioUrl = word.buildAudioUri()
+            logD("builtAudioUrl: $builtAudioUrl")
+
             setDataSource(word.buildAudioUri())
 
             prepareAsync()
@@ -180,7 +185,9 @@ class CardAudioPlayer @Inject constructor(
     }
 
     private fun String.buildAudioUri(): String {
-        return AUDIO_URI_TEMPLATE.format(this.trim().lowercase())
+        val encodedText = Uri.encode(this.trim().lowercase())
+
+        return AUDIO_URI_TEMPLATE.format(encodedText)
     }
 
     private fun resetPreparingJob() {
